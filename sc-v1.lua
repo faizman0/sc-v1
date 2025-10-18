bbf8b3cb9e70490f9ec49a2d8b258cc9b7f5752d
faizman0 <auliahersa@gmail.com>
2025-10-18 15:01:57 +0700

    add name

--- sc-v1.lua
+++ sc-v1.lua
@@ -396,7 +396,7 @@ local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 -- })
 
 local Window = Rayfield:CreateWindow({
-   Name = "Streetlife",
+   Name = "Faiz Ni Boss Senggol Dong",
    Icon = 0,
    LoadingTitle = "Faiz",
    LoadingSubtitle = "by Faiz ni boss",
